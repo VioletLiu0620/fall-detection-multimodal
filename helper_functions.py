a49f2ff0b88a80884ed7442a12c616d0c22f125b
VioletLiu0620 <liuviolet620@gmail.com>
2026-08-21 04:31:48 +0800

    change name to helper_functions, def function mov_to_csv for one specific video

---
+++ helper_functions.py
@@ -0,0 +1,56 @@
+from ultralytics import YOLO
+import numpy as np
+import pandas as pd
+
+model = YOLO("yolov8n-pose.pt")
+
+def mov_to_csv(video_name: str, video_type: str = ".mov"):
+    """
+    Parameters:
+    video_name: name of the video file without .mov or .mp4
+    video_type: .mov or .mp4, default to .mov 
+
+    Returns a csv file that has the same name as video_name.csv with columns "Frame", "Keypoint", "X", "Y", "Confidence"
+    """
+    full_video_path = f"{video_name}{video_type}"
+    print(f"Processing video file {full_video_path} to csv")
+
+    results = model(full_video_path, stream= True, verbose= False) # stream= True reduce memory consumption, verbose= False to stop printing long detection message
+
+    result_list = []
+    joint_names = [
+        "Nose", "Left Eye", "Right Eye", "Left Ear", "Right Ear",
+        "Left Shoulder", "Right Shoulder", "Left Elbow", "Right Elbow",
+        "Left Wrist", "Right Wrist", "Left Hip", "Right Hip",
+        "Left Knee", "Right Knee", "Left Ankle", "Right Ankle"
+    ]
+
+    # Flatten version
+    for frame_idx, result in enumerate(results):
+        kpts = result.keypoints.data # (num_ppl, 17, 3)
+        num_ppl = kpts.shape[0] # DON'T DO kpts[0].shape -> this will error and kpts[0] gives you the first object in the array
+
+        if num_ppl == 0:
+            continue
+
+        flat_kpts = kpts.reshape(-1, 3).cpu().numpy() # from (num_ppl, 17, 3) but now treat it as (num_ppl*17, 3), flatten to 2D from a 3D matrix
+        df = pd.DataFrame(flat_kpts, columns= ["X", "Y", "Confidence"])
+
+        df["Frame"] = frame_idx + 1
+        df["Keypoint"] = np.tile(joint_names, num_ppl) # repeat (joint_names) for (num_ppl) times
+
+        # Rearrange
+        df = df[["Frame", "Keypoint", "X", "Y", "Confidence"]]
+
+        result_list.append(df)
+
+    output_csv_name = f"{video_name}.csv"
+
+    if result_list:
+        final_df = pd.concat(result_list, ignore_index= True)
+        final_csv = final_df.to_csv(output_csv_name, index=False) # to_csv is a pd function that turn a Dataframe into a csv file
+
+    print(f"Outputing {output_csv_name}")
+
+    return final_csv
+
