acabbbed1fc03e3f4fc6d9db603f001f43c63e89
VioletLiu0620 <liuviolet620@gmail.com>
2026-08-21 05:40:16 +0800

    finish csv_to_pred_label function for one csv file

--- helper_functions.py
+++ helper_functions.py
@@ -1,16 +1,21 @@
 from ultralytics import YOLO
 import numpy as np
 import pandas as pd
+import torch
+from torch import nn
+import pathlib
+from pathlib import Path
+from preprocess import process_one_video
 
 model = YOLO("yolov8n-pose.pt")
 
-def mov_to_csv(video_path: str):
+def mov_to_csv(video_path: str|Path):
     """
-    Parameters:
-    video_name: name of the video file without .mov or .mp4
-    video_type: .mov or .mp4, default to .mov 
+    Args:
+        video_path (str | Path): full video path name, including the extension .mov or .mp4
 
-    Returns a csv file that has the same name as video_name.csv with columns "Frame", "Keypoint", "X", "Y", "Confidence"
+    Returns:
+        the name of a csv file with columns "Frame", "Keypoint", "X", "Y", "Confidence"
     """
     print(f"Processing video file {video_path} to csv")
 
@@ -52,6 +57,43 @@ def mov_to_csv(video_path: str):
 
     print(f"Outputing {output_csv_name}")
 
-    return final_csv
+    return output_csv_name
 
-mov_to_csv("c_f_c_a.MOV")
+
+def csv_to_pred_label(csv_file: str | Path,
+                      model: nn.Module,
+                      state_dict_path: str | Path,
+                      device: str | torch.device = "cpu" ):
+    """
+    Args:
+        csv_file (str | Path): str or path of the csv file
+        model (nn.Module): any PyTorch model for classification
+        state_dict_path (str | Path): str or path of the stored state_dict values (weight & biases)
+        device (str | Path): device that the model be on, default to cpu
+
+    Returns:
+        pred_label (str): the label that model predicted on - "Fall" or "No Fall"
+    """
+    label_map = {0: "Fall",
+                1: "No Fall"}
+    
+    arr = process_one_video(video_csv= pd.read_csv(csv_file))
+    X = torch.from_numpy(arr).float().unsqueeze(dim=0).to(device)
+
+    model.to(device)
+    model.load_state_dict(torch.load(state_dict_path))
+
+    model.eval()
+    with torch.inference_mode():
+        y_logits = model(X)
+        y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)
+        y_label = label_map[y_pred.item()]
+
+    file_path = Path(csv_file) # doesn't matter whether csv_file is a string or path, filename becomes a path object
+    filename = file_path.stem
+
+    print(f"The file {filename} is classified as {y_label}")
+
+    return y_label
+
+
